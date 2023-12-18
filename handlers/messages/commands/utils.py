23f09eff14526f428c746beef2d59240fe630373
1nfernoS <gaydenko01@yandex.ru>
2023-12-18 13:18:14 +0300

    Added another alias for test

--- handlers/messages/commands/utils.py
+++ handlers/messages/commands/utils.py
@@ -2,6 +2,6 @@ from vkbottle.tools.dev.mini_types.bot import MessageMin
 
 from bot_engine import labeler
 
-@labeler.message(text='pong')
+@labeler.message(text=['pong', 'понг'])
 async def ping(msg: MessageMin):
     await msg.answer('ya jivoy xd')
