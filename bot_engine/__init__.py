7888a36b0c1e9da1d135f6e3b25d308e865f2c00
1nfernoS <gaydenko01@yandex.ru>
2023-12-18 13:18:28 +0300

    Fixed recursive import error

--- bot_engine/__init__.py
+++ bot_engine/__init__.py
@@ -5,12 +5,12 @@ from config import group_token, ALLOWED_CHATS, IGNORE_LIST
 
 from .rules import WhiteListRule
 
-import handlers
-handlers.dummy()
-
 api = API(group_token)
 labeler = BotLabeler()
 labeler.auto_rules = [WhiteListRule(IGNORE_LIST, ALLOWED_CHATS)]
 
+
+import handlers
+handlers.dummy()
 
 bot = Bot(token=group_token, labeler=labeler)
