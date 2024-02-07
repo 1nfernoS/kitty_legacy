9fa8c0824f6ba12990177542d93e5e8185ef8065
1nfernoS <gaydenko01@yandex.ru>
2024-02-07 19:23:20 +0300

    Added some answers for ping

--- handlers/messages/commands/utils.py
+++ handlers/messages/commands/utils.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from vkbottle.tools.dev.mini_types.bot import MessageMin
 
 from bot_engine import labeler
@@ -10,7 +12,14 @@ from data_typings.enums import RoleAccess
 
 @labeler.message(AccessRule(RoleAccess.bot_access), text=['ping', 'пинг'])
 async def ping(msg: MessageMin):
-    await msg.answer('ya jivoy xd')
+    from random import choice
+    answers: List[str] = ["Я живой", "Да живой я", "Все в порядке", "понг", "Что-то случилось?", "Не стучи ><",
+                          "Внимание, анекдот", "Очередной текст на выбор", "Здесь могла быть ваша реклама",
+                          "Кстати, можете мне в ЛС предложить идею!", "Мой профиль", "Утро доброе", "Я не сплю",
+                          "Добрый день", "Добрый вечер", "Провел проверки, все работает", "Мяу", "Мяу :3", ":3",
+                          "М-р-р-р-р"]
+    await msg.answer(choice(answers))
+
 
 @labeler.message(AccessRule(RoleAccess.bot_access), text=['role', 'роль'])
 async def role(msg: MessageMin):
@@ -22,4 +31,3 @@ async def role(msg: MessageMin):
             user: User | None = s.query(User).filter(User.user_id == msg.from_id).first()
         user_role: Role = user.user_role
     await msg.answer(f'Ваша роль - {user_role.alias.capitalize()}')
-
