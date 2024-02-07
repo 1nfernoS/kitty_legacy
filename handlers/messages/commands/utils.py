e73a67137c20727449310cfbfba34ec268fe0411
1nfernoS <gaydenko01@yandex.ru>
2024-02-07 19:15:21 +0300

    Added role command

--- handlers/messages/commands/utils.py
+++ handlers/messages/commands/utils.py
@@ -11,3 +11,15 @@ from data_typings.enums import RoleAccess
 @labeler.message(AccessRule(RoleAccess.bot_access), text=['ping', 'пинг'])
 async def ping(msg: MessageMin):
     await msg.answer('ya jivoy xd')
+
+@labeler.message(AccessRule(RoleAccess.bot_access), text=['role', 'роль'])
+async def role(msg: MessageMin):
+    with session() as s:
+        user: User | None = s.query(User).filter(User.user_id == msg.from_id).first()
+        if not user:
+            s.add(User(user_id=msg.from_id))
+            s.commit()
+            user: User | None = s.query(User).filter(User.user_id == msg.from_id).first()
+        user_role: Role = user.user_role
+    await msg.answer(f'Ваша роль - {user_role.alias.capitalize()}')
+
