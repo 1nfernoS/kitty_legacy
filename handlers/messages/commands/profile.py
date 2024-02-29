edb8e4835e80e50601e4101d7fdc7677aec9b76d
1nfernoS <gaydenko01@yandex.ru>
2024-02-29 18:02:54 +0300

    Added build command

--- handlers/messages/commands/profile.py
+++ handlers/messages/commands/profile.py
@@ -1,17 +1,21 @@
-from typing import List, Tuple
+from typing import List
 
 from vkbottle.tools.dev.mini_types.bot import MessageMin
 from vkbottle_types.objects import MessagesSendUserIdsResponseItem
 
-import profile_api
+from config import DISCOUNT_PERCENT
+from data_typings.enums import RoleAccess
+from utils.math import commission_price, discount_price
+from utils.formatters import format_profile_skills
+
 from bot_engine import labeler, api
 from bot_engine.rules import AccessRule
 
-from ORM import session, Item
-from config import DISCOUNT_PERCENT
+import profile_api
+from profile_api.user_requests import get_profile, lvl_skills
+from profile_api.formatters import get_books, get_build
 
-from data_typings.enums import RoleAccess
-from utils.math import commission_price, discount_price
+from ORM import session, Item, User
 
 
 async def _item_price_base(msg: MessageMin, item: str, count: int = 1) -> MessagesSendUserIdsResponseItem:
@@ -61,3 +65,40 @@ async def item_price(msg: MessageMin, item: str):
     return await _item_price_base(msg, item)
 
 
+@labeler.message(AccessRule(RoleAccess.profile_app), text=['билд', 'build', 'экип', 'equip'])
+async def item_price(msg: MessageMin):
+    msg_to_edit = await msg.answer('Поднимаю записи...')
+
+    with session() as s:
+        auth: str | None = s.query(User.profile_key).filter(User.user_id == msg.from_id).first()[0]
+    if not auth:
+        message = "Сдайте ссылку на профиль мне в лс!\n" \
+                  "Проще всего это сделать через сайт, скопировав адрес ссылки кнопки 'Профиль' в приложении.\n" \
+                  "Если получилась ссылка формата 'https:// vip3.activeusers .ru/блаблабла', то все получится)"
+        return await msg.answer(message)
+
+    profile = await get_profile(auth, msg.from_id)
+    inv = [int(i) for i in profile['items']]
+    books = get_books(inv)
+    build = get_build(inv)
+    skills = await lvl_skills(auth, msg.from_id)
+
+    with session() as s:
+        user: User = s.query(User).filter(User.user_id == msg.from_id).first()
+        user.user_items = [s.query(Item).filter(Item.id == i).first()
+                           for i in books]
+        s.add(user)
+        s.commit()
+    user_name = await api.users.get(msg.from_id, name_case="gen")
+    user_name = f"[id{user_name[0].id}|{user_name[0].first_name}]"
+    message = f'Билд {user_name}:'
+    if build['books']:
+        message += '\nКниги:'
+        message += format_profile_skills(build['books'], skills)
+
+    if build['adms']:
+        message += '\nВ адмах:'
+        message += format_profile_skills(build['adms'], skills)
+
+    return await api.messages.edit(msg_to_edit.peer_id, message,
+                                   conversation_message_id=msg_to_edit.conversation_message_id)
---
+++ utils/formatters.py
@@ -0,0 +1,24 @@
+from ORM import session, Item
+from data_typings.profile import Skills
+
+
+def format_profile_skills(item_list: list, skills: Skills) -> str:
+    message = ''
+    s = session()
+
+    for book in item_list:
+        b_item: Item = s.query(Item).filter(Item.id == book).first()
+
+        lvl = None
+        if "(А) " in b_item.name:
+            # book_name = b_item.name.replace("(А) ", f"{tab}{active_book}")
+            lvl = [skill['level'] for skill in skills['active'] if b_item.name[4:].startswith(skill['name'][:-1])]
+
+        if "(П) " in b_item.name:
+            # book_name = b_item.name.replace("(П) ", f"{tab}{passive_book}")
+            lvl = [skill['level'] for skill in skills['passive'] if b_item.name[4:].startswith(skill['name'][:-1])]
+        message += '\n' + f'{b_item.name}'
+        if lvl:
+            message += f" - {lvl[0]} ({(round((int(lvl[0]) / 10) ** 0.5 * 10) + 100)}%)"
+    s.close()
+    return message
