5760e40ea248c2f4e73e3b84231b4f6ff0b65cda
1nfernoS <no_mail@mail.com>
2024-03-04 01:14:54 +0300

    Added puzzle answers and rule for check forward from bot

--- bot_engine/rules.py
+++ bot_engine/rules.py
@@ -5,16 +5,19 @@ from vkbottle.tools.dev.mini_types.base import BaseMessageMin
 
 from data_typings.enums import RoleAccess
 
+from config import PIT_BOT
+
 
 class WhiteListChatRule(ABCRule[BaseMessageMin]):
     """
     Rule to check from where comes message.
     True if user not in ignore_users and chat in allowed_chats
     """
+    
     def __init__(self, allowed_chats: Sequence[int]):
         self.allowed_chats = allowed_chats
         return
-
+    
     async def check(self, event: BaseMessageMin) -> bool:
         return event.peer_id - 2e9 in self.allowed_chats
 
@@ -24,6 +27,7 @@ class AccessRule(ABCRule[BaseMessageMin]):
     Rule to check access type for user's role
     return Role access
     """
+    
     def __init__(self, require_access: RoleAccess):
         self.require = require_access.value
         return
@@ -48,10 +52,11 @@ class FwdOrReplyUserRule(ABCRule[BaseMessageMin]):
     """
     Rule to check if message has Forward or Reply from user
     """
+    
     def __init__(self, allow_self: bool = True):
         self.allow_self = allow_self
         return
-
+    
     async def check(self, event: BaseMessageMin) -> bool:
         if event.reply_message:
             check_self = event.reply_message.from_id == event.from_id if not self.allow_self else True
@@ -59,4 +64,36 @@ class FwdOrReplyUserRule(ABCRule[BaseMessageMin]):
         if event.fwd_messages:
             check_self = event.fwd_messages[0].from_id == event.from_id if not self.allow_self else True
             return event.fwd_messages[0].from_id > 0 and check_self
+        return False
+
+
+class FwdPitRule(ABCRule[BaseMessageMin]):
+    """
+    Rule to check if message is Forwarded from pit
+    """
+    
+    def __init__(self, only_first: bool = True):
+        self.only_first = only_first
+        return
+    
+    async def check(self, event: BaseMessageMin) -> bool:
+        if event.fwd_messages:
+            return (event.fwd_messages[0].from_id == PIT_BOT and
+                    len(event.fwd_messages) == 1) if self.only_first else True
+        return False
+
+
+class FwdPitRule(ABCRule[BaseMessageMin]):
+    """
+    Rule to check if message is Forwarded from pit
+    """
+    
+    def __init__(self, only_first: bool = True):
+        self.only_first = only_first
+        return
+    
+    async def check(self, event: BaseMessageMin) -> bool:
+        if event.fwd_messages:
+            return (event.fwd_messages[0].from_id == PIT_BOT and
+                    len(event.fwd_messages) == 1) if self.only_first else True
         return False
