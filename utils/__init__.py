8877f609c356f2ae3f5a4209d97a8fd3712cf747
1nfernoS <gaydenko01@yandex.ru>
2024-03-12 14:22:23 +0300

    Added files in module

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
 from .datetime import now
-
-__all__ = ('now',)
+from .formatters import format_name, format_profile_skills
+from .math import commission_price, discount_price, pure_price
--- utils/formatters.py
+++ utils/formatters.py
@@ -1,33 +1,31 @@
 from typing import Literal
 
-from ORM import session, Item
-from bot_engine import api
 from data_typings.profile import Skills
 from data_typings.emoji import tab, active_book, passive_book
 
 
 def format_profile_skills(item_list: list, skills: Skills) -> str:
+    from ORM import session, Item
     message = ''
-    s = session()
+    with session() as s:
+        for book in item_list:
+            b_item: Item = s.query(Item).filter(Item.id == book).first()
 
-    for book in item_list:
-        b_item: Item = s.query(Item).filter(Item.id == book).first()
+            lvl = None
+            if "(А) " in b_item.name:
+                book_name = b_item.name.replace("(А) ", f"{tab}{active_book}")
+                lvl = [skill['level'] for skill in skills['active'] if b_item.name[4:].startswith(skill['name'][:-1])]
 
-        lvl = None
-        if "(А) " in b_item.name:
-            book_name = b_item.name.replace("(А) ", f"{tab}{active_book}")
-            lvl = [skill['level'] for skill in skills['active'] if b_item.name[4:].startswith(skill['name'][:-1])]
-
-        if "(П) " in b_item.name:
-            book_name = b_item.name.replace("(П) ", f"{tab}{passive_book}")
-            lvl = [skill['level'] for skill in skills['passive'] if b_item.name[4:].startswith(skill['name'][:-1])]
-        message += '\n' + f'{book_name}'
-        if lvl:
-            message += f" - {lvl[0]} ({(round((int(lvl[0]) / 10) ** 0.5 * 10) + 100)}%)"
-    s.close()
+            if "(П) " in b_item.name:
+                book_name = b_item.name.replace("(П) ", f"{tab}{passive_book}")
+                lvl = [skill['level'] for skill in skills['passive'] if b_item.name[4:].startswith(skill['name'][:-1])]
+            message += '\n' + f'{book_name}'
+            if lvl:
+                message += f" - {lvl[0]} ({(round((int(lvl[0]) / 10) ** 0.5 * 10) + 100)}%)"
     return message
 
 
 async def format_name(user_id: int, case: Literal["nom", "gen", "dat", "acc", "ins", "abl"] = 'gen') -> str:
+    from bot_engine import api
     user = await api.users.get(user_id, name_case=case)
     return f"[id{user[0].id}|{user[0].first_name}]"
