c24267786c922280ea9acb0849cf59f5b1023511
1nfernoS <gaydenko01@yandex.ru>
2024-03-25 07:39:58 +0300

    Added scatter commission

--- utils/math.py
+++ utils/math.py
@@ -1,17 +1,17 @@
-from config import DISCOUNT_PERCENT, COMMISSION_PERCENT
+from config import DISCOUNT_PERCENT, GOLD_COMMISSION_PERCENT
 
 
 def multiplier_percent(percent: int) -> float:
     return 100/(100-percent)
 
 
-def commission_price(money: int) -> int:
-    res = money * multiplier_percent(COMMISSION_PERCENT)
+def commission_price(money: int, percentage: int = GOLD_COMMISSION_PERCENT) -> int:
+    res = money * multiplier_percent(percentage)
     return round(res) if round(res) == res else int(res)+1
 
 
-def pure_price(money: int) -> int:
-    res = money / multiplier_percent(COMMISSION_PERCENT)
+def pure_price(money: int, percentage: int = GOLD_COMMISSION_PERCENT) -> int:
+    res = money / multiplier_percent(percentage)
     return round(res) if round(res) == res else int(res)
 
 
