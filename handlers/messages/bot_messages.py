d3bcd87257d26b2cee0fe8b54b7e80a576b91530
1nfernoS <gaydenko01@yandex.ru>
2024-03-25 07:33:24 +0300

    Added logs for take and put gold

--- handlers/messages/bot_messages.py
+++ handlers/messages/bot_messages.py
@@ -1,9 +1,11 @@
 from vkbottle.tools.dev.mini_types.bot import MessageMin
 
-from ORM import session, LogsItems, Item
+from ORM import session, LogsItems, Item, LogsMoney, User
 from bot_engine import labeler
 from bot_engine.rules import OverseerRule
-from data_typings.enums import ItemAction
+from data_typings.enums import ItemAction, ChangeMoneyAction
+from resources.emoji import gold
+from utils.formatters import balance_message_addition
 
 
 @labeler.chat_message(OverseerRule(f"&#<emo1>;[id<user_id:int>|<name>], Вы положили на склад: &#<emo2>;<count:int>*<item_name>!\n"
@@ -39,3 +41,27 @@ async def transfer_log(msg: MessageMin, id_from: int, id_to: int, item_name: str
         return  # TODO: log error
     LogsItems(id_from, ItemAction.GIVE, id_to, item.id, count).make_log()
     return
+
+
+@labeler.chat_message(OverseerRule(f"{gold}[id<user_id>|<name>], Вы взяли <count:int> золота из казны."))
+async def money_take(msg: MessageMin, user_id: int, count: int):
+    LogsMoney(user_id, ChangeMoneyAction.SUB, count, 0, 'FROM STORAGE').make_log()
+    with session() as s:
+        user: User = s.query(User).filter(User.user_id == user_id).first()
+        user.balance -= count
+        s.add(user)
+        s.commit()
+        answer = f"О, [id{user_id}|Вы] взяли {count} золота!\n" + balance_message_addition(user.balance)
+    return await msg.answer(answer)
+
+
+@labeler.chat_message(OverseerRule(f"{gold}[id<user_id>|<name>], Вы положили <count:int> золота в казну (комиссия 10%)."))
+async def money_put(msg: MessageMin, user_id: int, count: int):
+    LogsMoney(user_id, ChangeMoneyAction.ADD, count, 0, 'TO STORAGE').make_log()
+    with session() as s:
+        user: User = s.query(User).filter(User.user_id == user_id).first()
+        user.balance += count
+        s.add(user)
+        s.commit()
+        answer = f"О, [id{user_id}|Вы] положили {count} золота!\n" + balance_message_addition(user.balance)
+    return await msg.answer(answer)
