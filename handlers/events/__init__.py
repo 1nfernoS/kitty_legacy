dea9f8421c2f68a49597c1056f160d2c744f095e
Гайденко Михаил <gaydenko01@yandex.ru>
2024-05-04 10:07:33 +0300

    Updated buff event answer

--- handlers/events/__init__.py
+++ handlers/events/__init__.py
@@ -1,8 +1,10 @@
+from vkbottle import CtxStorage
 from vkbottle.tools.dev.mini_types.bot import MessageEventMin
 from vkbottle_types.events import GroupEventType
 
-from bot_engine import labeler, api
+from bot_engine import labeler, api, bot
 from bot_engine.rules import ActionEventRule
+from bot_engine.user_bot import buff_loop
 from data_typings.enums import EventPayloadAction
 
 
@@ -10,3 +12,20 @@ from data_typings.enums import EventPayloadAction
 async def remove(event: MessageEventMin):
     await api.messages.delete(cmids=event.conversation_message_id, peer_id=event.peer_id, delete_for_all=True)
     return await event.show_snackbar('Done!')
+
+
+@labeler.raw_event(GroupEventType.MESSAGE_EVENT, MessageEventMin, ActionEventRule(EventPayloadAction.BUFF))
+async def buff(event: MessageEventMin):
+    res = await event.show_snackbar('Hatova!')
+    if CtxStorage().contains(event.payload['data']['from_id']):
+        return await api.messages.edit(event.peer_id, 'Баффер занят, повторите позднее',
+                                       conversation_message_id=event.conversation_message_id)
+    else:
+        CtxStorage().set(
+            event.payload['data']['from_id'],
+            {'payload': event.payload['data'], 'event': event}
+        )
+
+    await bot.loop.create_task(buff_loop(event.payload['data']))
+
+    return res
