82b085d4b0250f7058889081e403c38f0e5ff27b
Гайденко Михаил <gaydenko01@yandex.ru>
2024-05-08 11:31:42 +0300

    Change imports variables from utils

--- bot_engine/tasks.py
+++ bot_engine/tasks.py
@@ -14,7 +14,6 @@ from ORM.utils import Task
 from data_typings.enums import guild_roles
 from utils import now
 from utils.formatters import format_name
-import utils
 from . import bot
 from resources import emoji
 
@@ -89,27 +88,28 @@ async def remind(params: RemindArgs):
                           peer_id=params['peer_id'],
                           is_reply=True).json()
 
-    return await api.messages.send(chat_id=GUILD_CHAT_ID,
-                                   random_id=0,
-                                   message=msg,
-                                   forward=fwd)
+    await api.messages.send(chat_id=GUILD_CHAT_ID,
+                            random_id=0,
+                            message=msg,
+                            forward=fwd)
+    return
 
 
 @bot.loop_wrapper.interval(seconds=2)
 async def check_tasks():
-    now = utils.now()
+    now_ = now()
     with session() as s:
         # noinspection PyTypeChecker
         task_list: List[Task] = s.query(Task).filter(Task.task_active == 1).all()
         for task in task_list:
-            if task.task_time_at > now:
+            if task.task_time_at > now_:
                 continue
             next_time = await globals()[task.task_exec_target](loads(task.task_args))
             if task.task_regular:
-                if not isinstance(next_time, timedelta):
+                if not isinstance(next_time, datetime):
                     raise AttributeError(
                         f"Task {task.task_exec_target} is regular, but no time for next call proceeded")
-                task.task_time_at = now + next_time
+                task.task_time_at = next_time
             else:
                 task.task_active = False
             s.add(task)
